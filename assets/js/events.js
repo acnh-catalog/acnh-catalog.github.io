12e12f9ddb0485ca3de7946618716a1fd927f791
Sasuke <sasuke.tsukishima@gmail.com>
2021-06-28 19:06:50 -0400

    adding flowers, trees, materials and etc

--- assets/js/events.js
+++ assets/js/events.js
@@ -2,37 +2,6 @@ const className = {
   active: 'is-active',
   selected: 'is-selected'
 },
-page = {
-  housewares: 'items-housewares.html',
-  misc: 'items-miscellaneous.html',
-  wallMounted: 'items-wallMounted.html',
-  tops: 'fashion-tops.html',
-  bottoms: 'fashion-bottoms.html',
-  dresses: 'fashion-dresses.html',
-  swimwear: 'fashion-swimwear.html',
-  headwear: 'fashion-headwear.html',
-  accessories: 'fashion-accessories.html',
-  socks: 'fashion-socks.html',
-  shoes: 'fashion-shoes.html',
-  bags: 'fashion-bags.html',
-  umbrellas: 'fashion-umbrellas.html',
-  wallpaper: 'house-wallpaper.html',
-  floors: 'house-floors.html',
-  rugs: 'house-rugs.html',
-  photos: 'npc-photos.html',
-  posters: 'npc-posters.html',
-  villagers: 'npc-villagers.html',
-  insects: 'catch-insects.html',
-  fish: 'catch-fish.html',
-  seaCreatures: 'catch-seaCreatures.html',
-  music: 'others-music.html',
-  fossils: 'others-fossils.html',
-  art: 'others-art.html',
-  tools: 'diy-tools.html',
-  fencing: 'diy-fencing.html',
-  materials: 'diy-materials.html',
-  recipes: ''
-},
 element = {
   nav: {
     main: '#js-main-nav',
@@ -62,15 +31,6 @@ function updateSeletedMenu(menu) {
   $(element.nav.source).find(element.menu).removeClass(className.selected);
   $this.addClass(className.selected);
 }
-
-function addSelectedMenuState() {
-  let href = window.location.href,
-      index = href.lastIndexOf('/') + 1,
-      filename = href.substr(index);
-  $(element.nav.main).find(element.menu+'[href="'+filename+'"]').addClass(className.selected);
-}
-
-addSelectedMenuState();
 
 $(element.nav.source).on('click', element.menu, function(event) {
   event.preventDefault();
